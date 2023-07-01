b4b44efaefa314b6452d446b946979239ee6ceff
BorgorKang <borgorkang@proton.me>
2023-06-30 21:15:41 -0600

    implement image carousel w/ placeholder images

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 // Wait for the DOM to fully load
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the scrollable element by its id
-    const scrollableElement = document.getElementById('myScrollableBox');
-    // Create a new SimpleBar instance
-    new SimpleBar(scrollableElement);
-  });
-  
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the scrollable element by its id
+  const scrollableElement = document.getElementById("myScrollableBox");
+  // Create a new SimpleBar instance
+  new SimpleBar(scrollableElement);
+});
+
+var slideIndex = 0;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+  showSlide((slideIndex += n));
+}
+
+function showSlide(n) {
+  var slides = document
+    .getElementsByClassName("slides")[0]
+    .getElementsByTagName("img");
+
+  if (n >= slides.length) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = slides.length - 1;
+  } else {
+    slideIndex = n;
+  }
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+}
+
